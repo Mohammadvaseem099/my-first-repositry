de5d8baec0236b953651da107b73891ce0609db1
Mohammad Waseem <mohammadvaseem099@gmail.com>
2022-08-11 23:41:53 +0530

    post request-players

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,9 +2,62 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
 
 module.exports = router;
 // adding this comment for no reason
+
+
+let players = [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
+
+router.post('/players', function(req, res){
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    for(let i=0; i<players.length; i++){
+        if(players[i].name == newPlayersName){
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    if(isNameRepeated){
+        res.send("This player was already added")
+    } else {
+        players.push(newPlayer)
+        res.send({data:players,status:true})
+        
+    }
+    })
+
+
+
+module.exports = router;
