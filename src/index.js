fb3674c0eab0341a823beb9e1bdc847749b898be
Mohammad Waseem <mohammadvaseem099@gmail.com>
2022-08-14 15:58:34 +0530

    Schema ass.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-
 const route = require('./routes/route.js');
+const mongoose = require('mongoose')
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb+srv://Mohammadvaseem099:uDNTAkafkNrYLe0C@cluster0.2npclft.mongodb.net/Mohammadvaseem099",{
+useNewUrlParser: true
+})
+.then( () => console.log("Mongoose is connected"))
+.catch ( err => console.log(err))
+
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3001, function() {
+    console.log(`Express app running on port ${process.env.PORT || 3001}`)
 });
