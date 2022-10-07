7ec897e6d896186f5b35c871ba2f20cba98d0da0
Meenakshi Lodhi Rajput <105542143+msuryavanshi982@users.noreply.github.com>
2022-10-07 10:43:02 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,5 +8,10 @@ router.post('/url/shorten',urlController.shortUrl);
 //============================get api for redirect url===================>>>
 router.get('/:urlCode',urlController.getUrl);
 
+//=====This API is used for handling any invalid Endpoints=====>>>>>
+router.all("/*", async function (req, res) {
+  res.status(404).send({ status: false, msg: "Page Not Found!!!" });
+});
+
 
 module.exports = router
